a72f9719b16aefc7d0202686257e9a7948dd82cf
yinheli <me@yinheli.com>
2022-11-06 03:50:03 +0800

    optimize: build.rs

--- src/build.rs
+++ src/build.rs
@@ -3,6 +3,7 @@ use std::env;
 
 fn main() {
     println!("cargo:rerun-if-changed=assets/*");
-    copy_to_output("assets", &env::var("PROFILE").unwrap()).expect("Could not copy");
-    copy_to_output("README.md", &env::var("PROFILE").unwrap()).expect("Could not copy");
+    let build_type = &env::var("PROFILE").unwrap();
+    copy_to_output("assets", build_type).expect("Could not copy");
+    copy_to_output("README.md", build_type).expect("Could not copy");
 }
